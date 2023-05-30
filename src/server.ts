c643486bcb2f33478f2f70b0f8c0a6204605278f
Kshitij <kshitij.mahajan.20042@iitgoa.ac.in>
2023-05-31 00:16:03 +0530

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,60 @@
+import process from "process";
+import express, {Express, Request, Response} from "express";
+import bodyParser from "body-parser";
+import axios, {AxiosResponse} from "axios";
+
+type EAInput ={
+    id: number | string;
+    data: {
+        number: number | string
+        infoType: string;
+    }
+}
+
+type EAOutput = {
+    jobRunId: string | number;
+    statusCode: number;
+    data : {
+        result?: any
+    }
+    error?: string
+}
+const PORT = process.env.PORT || 8080
+const app: Express = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("External Adapter says Hello");
+});
+
+app.post("/", async (req: Request, res: Response)=>{
+    //res.send("Helloo");
+    const eaInputData: EAInput = req.body;
+    console.log(" Request data received: ", eaInputData);
+    // Built the API request to look something like http://numbersapi.com/random/trivia
+
+    const url = `http://numbersapi.com/${eaInputData.data.number}/${eaInputData.data.infoType}`;
+    // Build EA's response
+    let eaResponse: EAOutput = {
+    data: {},
+    jobRunId: eaInputData.id,
+    statusCode: 0,
+    };
+    try {
+        const apiResponse: AxiosResponse = await axios.get(url);
+        eaResponse.data = { result: apiResponse.data };
+        eaResponse.statusCode = apiResponse.status;
+        console.log("Returned:", eaResponse)
+        res.json (eaResponse);
+    } catch (error: any) {
+        console.log("API Response error: ", error);
+        eaResponse.error = error.message;
+        eaResponse.statusCode = error.response.status;
+        res.json (eaResponse);
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}.`);
+});
